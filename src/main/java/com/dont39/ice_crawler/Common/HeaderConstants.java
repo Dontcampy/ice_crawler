eb7cd963d5e09cc74f7d3d050612cb5bc3272e47
Dontcampy <dontcampy@gmail.com>
2019-04-01 23:07:46 +0800

    添加MD5生成工具

--- src/main/java/com/dont39/ice_crawler/Common/HeaderConstants.java
+++ src/main/java/com/dont39/ice_crawler/Common/HeaderConstants.java
@@ -2,7 +2,7 @@ package com.dont39.ice_crawler.Common;
 
 import java.util.ResourceBundle;
 
-public class Header {
+public class HeaderConstants {
     private static ResourceBundle bundle = ResourceBundle.getBundle("setting");
     public static String USERAGENT = bundle.getString("header_userAgent");
     public static String COOKIE = bundle.getString("header_cookie");
--- src/main/java/com/dont39/ice_crawler/Engine/HttpClientThread.java
+++ src/main/java/com/dont39/ice_crawler/Engine/HttpClientThread.java
@@ -1,27 +1,31 @@
 package com.dont39.ice_crawler.Engine;
 
+import com.dont39.ice_crawler.Common.HeaderConstants;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 
+public class HttpClientThread implements Runnable{
+    private String url;
 
-import java.io.IOException;
+    public HttpClientThread(String url) {
+        this.url = url;
+    }
 
-public class Crawler {
-    public static void main(String[] args) {
+    @Override
+    public void run() {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://www.ctolib.com/topics-80581.html");
+        HttpGet httpGet = new HttpGet(url);
         RequestConfig requestConfig = RequestConfig.custom()
                 .setSocketTimeout(20)
                 .setConnectTimeout(20).build();
         httpGet.setConfig(requestConfig);
-        httpGet.setHeader("User-Agent:", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36");
+        httpGet.setHeader("User-Agent:", HeaderConstants.USERAGENT);
 
         try {
             CloseableHttpResponse response = httpClient.execute(httpGet);
